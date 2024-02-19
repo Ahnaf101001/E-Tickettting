3ee79b346227d0d251841d547a94828d293d729a
Ahnaf Khan <ahnaf10100@gmail.com>
2024-02-19 23:41:42 +0600

    done

--- scripts/eticketing.js
+++ scripts/eticketing.js
@@ -13,22 +13,54 @@ function home(){
     hideElementById("seatsBookingPage");
 }
 
-
+let seat = 40;
 let count = 0;
 let money = 0;
 
 function selected(elementId){
     count++;
+    seat--;
     money = money + 550;
 
     if(count<=4){
         setBackGround(elementId);
+
+
+    const totalTickets = document.getElementById("totalTickets")
+    let seatNum = document.createElement('span')
+    let className = document.createElement('span')
+    let priceTag = document.createElement('span')
+    let lineBreak = document.createElement("br");
+
+    className.style.marginLeft = '150px'
+    priceTag.style.marginLeft = '70px'
+    
+    seatNum.innerText = elementId;
+    className.innerText = "Economoy";
+    priceTag.innerText = "550";
+    lineBreak.innerText = '<br>';
+
+    totalTickets.appendChild(seatNum);
+    totalTickets.appendChild(className);
+    totalTickets.appendChild(priceTag);
+    totalTickets.appendChild(lineBreak);
+
+
+
     }
     else{
         count = count - 1;
         money = money - 550;
+        seat = seat + 1;
         alert("Limit exceed");
     }
+
+    const display1 = document.getElementById("seatCount");
+    let displayString1 = display1.innerText;
+    let displayValue1 = parseInt(displayString1);
+    displayValue1 =  seat;
+
+    setTextElementValueById("seatCount", displayValue1);
 
     const display = document.getElementById("bookedSeatsCount");
     let displayString = display.innerText;
@@ -44,6 +76,9 @@ function selected(elementId){
 
     setTextElementValueById("totalMoney", displayValue2);
 
+    
+    
+    
     return count,money;
 
 }
